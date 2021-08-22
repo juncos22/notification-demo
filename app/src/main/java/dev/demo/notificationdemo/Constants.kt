e9bd2b2e3c46974ef44372fe4e034880c7c52341
juncos22 <juncosnicolas36@gmail.com>
2021-08-22 17:20:34 -0300

    Removed keys

--- app/src/main/java/dev/demo/notificationdemo/Constants.kt
+++ app/src/main/java/dev/demo/notificationdemo/Constants.kt
@@ -3,7 +3,7 @@ package dev.demo.notificationdemo
 class Constants {
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAF2qicy0:APA91bGfzhIJtDSXKqAGpZ8XOAA8wTrnHfUC1oQmSQHGNBwom6GWlLC3Kr1hEzmy6zZjhebDNxiARdYar6YmV9lluiwRwkwgjNa57zxSIKnnpKHe8kH4ODu14G8oZL_v66ES-P2L21Qp"
+        const val SERVER_KEY = "SERVER_KEY"
         const val CONTENT_TYPE = "application/json"
     }
 }
